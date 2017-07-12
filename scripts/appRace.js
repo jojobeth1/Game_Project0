d636832368cfd2519568364d668d6efb6220af70
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-07-11 23:37:14 -0700

    added color changing bg. Added instructions to html.

--- scripts/appRace.js
+++ scripts/appRace.js
@@ -4,85 +4,51 @@ var locationP1;
 var locationP2;
 
 $(document).ready(function(){
-
-
   //targeting the document window. On'keydown', variable "keyPlay" is passed through the function.
   $(document).keydown(function(keyPlay){
-    keyPlay.preventDefault();
     // controls for player 1.
-
-
-    if(keyPlay.keyCode === 65){
-      //console.log('A was pressed');
+        if(keyPlay.keyCode === 65){
       //if keyboard letter "A" is pressed, top left position of #theimage will move 100px over
-      $("#theimage").stop().animate({
-      left: '+=50'
-      });
-      //get's position for player 1
-      var locationP1 = $("#theimage").position();
-      x = parseInt(locationP1.left);
+          $("#theimage").stop().animate({left:'+=50'},"1000","swing")
+          //let p1x = $("#theimage").offset();
+          //console.log(p1x, "found") //886.5
+          //return $("#theimage").offset();
+          if ($("#theimage").offset.left >= 888.5){console.log("player 1 winner")}
+        }
 
-      //console.log(x);
-    }
 
     //controls for player 2
-    if(keyPlay.keyCode === 76){
-      //console.log('L was pressed');
-      $("#theimage2").stop().animate({
-      left: '+=50'
-      });
-      //get's position for player 2
-      var locationP2 = $("#theimage2").position();
-      //y = parseInt(locationP2.left);
-      //console.log(y);
-    }
-    //console.log("values", x,y);
+        if(keyPlay.keyCode === 76){
+        //console.log('A was pressed');
+        $("#theimage2").stop().animate({left:'+=50'},"1000","swing");
+          //var p2x = $("#theimage2").offset();
+          //console.log(p2x, "location found");
+        }
 
     //Controls for resting the game
-    if(keyPlay.keyCode === 32){
-      //resets player 1 to starting position
-      $("#theimage").css({
-        "position": "relative",
-        "left": "0"
-      });
-      $("#theimage2").css({
-        "position": "relative",
-        "left": "0"
-      });
-        console.log('space bar was pressed');
-      }
-
+        if(keyPlay.keyCode === 32){
+        //resets player 1 to starting position
+          $("#theimage").css({
+          "position": "relative",
+          "left": "0"
+          })
+          $("#theimage2").css({
+            "position": "relative",
+            "left": "0"
+          })
+          //console.log('space bar was pressed');
+        }
+        // win the game
+        //if (886.5
+        /*
+        if(p1x > p2x == true ){
+          $(".message").text("Player 1 is winning");
+        }else
+            {
+              $(".message").text("Player 2 is winning");
+            }
+        */
+    })
+})
     //console.log("x is", x);
   //  console.log("y is", y);
-
-    /*if(x>y){
-      $(".message").text("Player 1 is winning");
-    }else if (y<x)
-        {
-          $(".message").text("Player 2 is winning");
-        }
-    */
-  });
-})
-
-
-
-
-
-
-
-//function can be listener or handler
-
-//assign a keys a right, l is right.
-
-
-
-
-
-
-
-/*  $('document').keypress(function(e){if (e.which == 65){console.log("You pressed the right key");
-      $('#theimage').animate({left: '-= 10px'});
-    };
-  });
-*/
